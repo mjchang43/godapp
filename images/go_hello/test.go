1cc5cf5e25f02543f7c689fe6a3299f6fd8f1b4d
naruto <lpssft@hotmail.com>
2022-03-02 20:20:48 +0800

    Add images

---
+++ images/go_hello/test.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+  var s string
+  s = "how are you?"
+  fmt.Println(s)
+  s = `how are you?`
+  fmt.Println(s)
+
+  s = "<html>\n\t<body>\"Hello\"</body>\n</html>"
+  fmt.Println(s)
+
+  s = `
+<html>
+  <body>"Hello""</body>
+</html>`
+  fmt.Println(s)
+
+  fmt.Println("c:\\my\\dir\\file")
+  fmt.Println(`c:\my\dir\file`)
+}
